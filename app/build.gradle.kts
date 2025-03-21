2338a1b8c9ec0174528db657e088d193c05be4cc
Matheus <matheus.nobregamts@gmail.com>
2025-03-21 13:28:02 -0300

    Implementação do Firebase
    O processo contemplou a sincronização com a plataforma do firebase além das configurações internas no android studio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.meumts.projetologin"
-        minSdk = 21
+        minSdk = 31
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -27,8 +27,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
@@ -39,8 +39,25 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.common)
+    implementation(libs.credentials)
+    implementation(libs.credentials.play.services.auth)
+    implementation(libs.googleid)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(libs.firebase.bom)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.google.firebase.auth)
+
+    implementation(libs.credentials.v130)
+    implementation(libs.credentials.play.services.auth.v130)
+    implementation(libs.googleid)
+    implementation(libs.credentials.v120alpha04)
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/meumts/projetologin/FormLogin.java
+++ app/src/main/java/com/meumts/projetologin/FormLogin.java
@@ -1,6 +1,12 @@
 package com.meumts.projetologin;
 
+
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.os.CancellationSignal;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.content.Intent;
@@ -8,14 +14,30 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.content.ContextCompat;
+import androidx.credentials.Credential;
+import androidx.credentials.CredentialManager;
+import androidx.credentials.CredentialManagerCallback;
+import androidx.credentials.CustomCredential;
+import androidx.credentials.GetCredentialRequest;
+import androidx.credentials.GetCredentialResponse;
+import androidx.credentials.exceptions.GetCredentialException;
+
 import android.os.Build;
 
-import com.google.firebase.Firebase;
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption;
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.concurrent.Executors;
+
 
 public class FormLogin extends AppCompatActivity {
 
-
-
+    private FirebaseAuth mAuth;
+    private CredentialManager credentialManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +47,9 @@ public class FormLogin extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             // Altera a cor da status bar para a cor definida em colors.xml
             getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.black));
-
         }
 
-
-
-    // Referências diretas para os EditText
+        // Referências diretas para os EditText
         EditText loginEdit = findViewById(R.id.ConteinerItensLogin);
         EditText senhaEdit = findViewById(R.id.ConteinerItensSenha);
         AppCompatButton loginButton = findViewById(R.id.btn2);
@@ -51,6 +70,92 @@ public class FormLogin extends AppCompatActivity {
                     Toast.makeText(FormLogin.this, "Login ou senha inválidos", Toast.LENGTH_SHORT).show();
                 }
             }
-  });
-}
+        });
+
+
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+        credentialManager = CredentialManager.create(this);
+
+        Button btnGoogleSignIn = findViewById(R.id.btn1);
+        btnGoogleSignIn.setOnClickListener(view -> signInWithGoogle());
+
+        btnGoogleSignIn.setOnClickListener(view -> {
+            Log.d(TAG, "Botão de login do Google foi clicado.");
+            signInWithGoogle();
+        });
+    }
+
+    private void signInWithGoogle() {
+        GetGoogleIdOption googleIdOption = new GetGoogleIdOption.Builder()
+                .setFilterByAuthorizedAccounts(false)
+                .setServerClientId(getString(R.string.web_id))
+                .build();
+
+        GetCredentialRequest request = new GetCredentialRequest.Builder()
+                .addCredentialOption(googleIdOption)
+                .build();
+
+        credentialManager.getCredentialAsync(
+                FormLogin.this,  // Certifique-se de usar o contexto da Activity
+                request,
+                new CancellationSignal(),
+                Executors.newSingleThreadExecutor(),
+                new CredentialManagerCallback<>() {
+                    @Override
+                    public void onResult(GetCredentialResponse result) {
+                        // Processamento do resultado
+                        handleSignIn(result.getCredential());
+                    }
+
+                    @Override
+                    public void onError(GetCredentialException e) {
+                        // Tratamento de erro
+                        Log.e(TAG, "Couldn't retrieve user's credentials: " + e.getLocalizedMessage());
+                    }
+                }
+        );
+
+
+
+
+
+
+
+
+
+
+    }
+
+    private void handleSignIn(Credential credential) {
+        if (credential instanceof CustomCredential && "google.com".equals(credential.getType())) {
+            // Recuperando o GoogleIdToken
+            Bundle credentialData = ((CustomCredential) credential).getData();
+            GoogleIdTokenCredential googleIdTokenCredential = GoogleIdTokenCredential.createFrom(credentialData);
+
+            firebaseAuthWithGoogle(googleIdTokenCredential.getIdToken());
+        } else {
+            Log.w(TAG, "Credencial inválida ou não reconhecida.");
+            Toast.makeText(this, "Credencial inválida. Tente novamente.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Toast.makeText(this, "Login bem-sucedido: " + user.getDisplayName(), Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(this, "Falha no login!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
+
+
 }
