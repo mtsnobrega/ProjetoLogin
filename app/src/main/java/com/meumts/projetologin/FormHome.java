f76091147d93f96c53e227dc105bef0dc0ef32dd
Matheus <matheus.nobregamts@gmail.com>
2025-03-20 22:16:38 -0300

    Update FormHome.java

--- app/src/main/java/com/meumts/projetologin/FormHome.java
+++ app/src/main/java/com/meumts/projetologin/FormHome.java
@@ -1,7 +1,9 @@
 package com.meumts.projetologin;
 
 import android.os.Bundle;
-
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -15,10 +17,25 @@ public class FormHome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_form_home);
+
+        // Ajusta o padding conforme as barras de sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
+
+        // Referência ao ImageButton de engrenagem
+        ImageButton btnEngrenagem = findViewById(R.id.btnEngrenagem);
+
+        btnEngrenagem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cria o intent para voltar para a tela de login
+                Intent intent = new Intent(FormHome.this, FormLogin.class);
+                startActivity(intent);
+                finish();
+            }
+   });
+}
 }
